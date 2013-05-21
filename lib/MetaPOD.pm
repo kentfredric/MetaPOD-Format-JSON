b8d32bdf4fc1013c77e7bacaffbf9a0cc593362d
Kent Fredric <kentfredric@gmail.com>
2013-05-22 06:03:33 +1200

    Build results of d090e24 (on master)

---
+++ lib/MetaPOD.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+
+package MetaPOD;
+BEGIN {
+  $MetaPOD::AUTHORITY = 'cpan:KENTNL';
+}
+{
+  $MetaPOD::VERSION = '0.1.0';
+}
+
+# ABSTRACT: An evolution of POD
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MetaPOD - An evolution of POD
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 AUTHOR
+
+Kent Fredric <kentfredric@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
