29604b2def016a39d68dec109bfe28c646d959aa
Kent Fredric <kentfredric@gmail.com>
2013-05-31 22:45:30 +1200

    Dispatch the callback on =for segments

--- lib/MetaPOD/Extractor.pm
+++ lib/MetaPOD/Extractor.pm
@@ -189,6 +189,9 @@ sub add_segment {
   $segment->{start_line} = $start_line if defined $start_line;
 
   push @{ $self->segments }, $segment;
+  my $cb = $self->end_segment_callback;
+  $cb->($segment);
+
   return $self;
 }
 
