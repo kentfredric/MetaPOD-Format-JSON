1344100f598ae309785240f645eeaa679857d4b9
Kent Fredric <kentfredric@gmail.com>
2013-05-31 21:17:10 +1200

    Make the version the 'last' version if version is passed undef

--- lib/MetaPOD/Role/Format.pm
+++ lib/MetaPOD/Role/Format.pm
@@ -65,6 +65,7 @@ Default implementation compares values given verses the results from C<< $class-
 
 sub supports_version {
   my ( $class, $version ) = @_;
+  return [ $class->supported_versions ]->[-1] if not defined $version;
   if ( $version !~ /^v/msx ) {
     croak q{Version specification does not begin with a 'v'};
   }
@@ -75,7 +76,6 @@ sub supports_version {
   croak "Version $v not supported. Supported versions: " . join q{,}, $class->supported_versions;
 }
 
-requires 'new_collector';
 requires 'add_segment';
 
 1;
