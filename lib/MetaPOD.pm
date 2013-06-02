ebb5676914af7c3069b62603ff8d6af6695f495a
Kent Fredric <kentfredric@gmail.com>
2013-06-02 19:00:52 +1200

    Build results of 02291b6 (on master)

--- lib/MetaPOD.pm
+++ lib/MetaPOD.pm
@@ -6,7 +6,7 @@ BEGIN {
   $MetaPOD::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $MetaPOD::VERSION = '0.1.1';
+  $MetaPOD::VERSION = '0.1.2';
 }
 
 # ABSTRACT: An evolution of POD
@@ -27,7 +27,7 @@ MetaPOD - An evolution of POD
 
 =head1 VERSION
 
-version 0.1.1
+version 0.1.2
 
 =begin MetaPOD::JSON v1.0.0
 
