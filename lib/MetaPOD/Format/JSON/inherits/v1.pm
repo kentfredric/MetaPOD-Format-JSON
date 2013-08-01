7306ca822437f01665c506b50f628959eef73394
Kent Fredric <kentfredric@gmail.com>
2013-08-02 05:51:06 +1200

    move <inherits> interface to a role

--- lib/MetaPOD/Format/JSON/inherits/v1.pm
+++ lib/MetaPOD/Format/JSON/inherits/v1.pm
@@ -1,21 +1,22 @@
 use strict;
 use warnings;
 
-package MetaPOD::Format::JSON::inherits;
+package MetaPOD::Format::JSON::inherits::v1;
 
 # ABSTRACT: Implementation of JSON/inherits format component
 
 =begin MetaPOD::JSON v1.1.0
 
 {
-    "namespace":"MetaPOD::Format::JSON::inherits",
-    "interface":"single_class"
+    "namespace":"MetaPOD::Format::JSON::inherits::v1",
+    "interface":"role"
 }
 
 =end MetaPOD::JSON
 
 =cut
 
+use Moo::Role;
 use Carp qw(croak);
 
 =method add_v1
@@ -26,7 +27,7 @@ Spec v1 C<inherits> Implementation
 
 =cut
 
-sub add_v1 {
+sub add_inherits {
   my ( $self, $inherits, $result ) = @_;
   if ( defined $inherits and not ref $inherits ) {
     return $result->add_inherits($inherits);
