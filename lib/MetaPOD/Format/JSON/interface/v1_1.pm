05dff4f9639f9c4b4ced105d92dde315c20ecf86
Kent Fredric <kentfredric@gmail.com>
2013-08-02 05:51:30 +1200

    move <interface> implementation to a role

--- lib/MetaPOD/Format/JSON/interface/v1_1.pm
+++ lib/MetaPOD/Format/JSON/interface/v1_1.pm
@@ -1,46 +1,47 @@
 use strict;
 use warnings;
 
-package MetaPOD::Format::JSON::interface;
+package MetaPOD::Format::JSON::interface::v1_1;
 
 # ABSTRACT: Implementation of JSON/interface format component
 
 =begin MetaPOD::JSON v1.1.0
 
 {
-    "namespace":"MetaPOD::Format::JSON::interface",
-    "interface":"single_class"
+    "namespace":"MetaPOD::Format::JSON::interface::v1_1",
+    "interface":"role"
 }
 
 =end MetaPOD::JSON
 
 =cut
 
+use Moo::Role;
 use Carp qw(croak);
 
-=method supported_interfaces_v1_1
+=method supported_interfaces
 
 Spec v1.1 C<interface> value list.
 
-    my @valid_interface_tokens = MetaPOD::Format::JSON::interface->supported_interfaces_v1_1
+    my @valid_interface_tokens = __SOME_CLASS__->supported_interfaces
 
 =cut
 
-sub supported_interfaces_v1_1 {
+sub supported_interfaces {
   return qw( class role type_library exporter single_class function );
 }
 
-=method check_interface_v1_1
+=method check_interface
 
 Spec v1.1 C<interface> Implementation key checking routine
 
-    MetaPOD::Format::JSON::interface->check_interface_v1_1( $interface, $interface, $interface );
+    __SOME_CLASS__->check_interface( $interface, $interface, $interface );
 
 Simply goes C<bang> if C<$interface> is not in C<supported_interfaces_v1_1>
 
 =cut
 
-sub check_interface_v1_1 {
+sub check_interface {
   my ( $self, @ifs ) = @_;
   my $supported = { map { ( $_, 1 ) } $self->supported_interfaces_v1_1 };
   for my $if (@ifs) {
@@ -51,15 +52,15 @@ sub check_interface_v1_1 {
   return $self;
 }
 
-=method add_v1_1
+=method add_interface
 
 Spec v1.1 C<interface> Implementation
 
-    MetaPOD::Format::JSON::interface->add_v1( $data->{interface} , $metapod_result );
+    __SOME_CLASS->add_interface( $data->{interface} , $metapod_result );
 
 =cut
 
-sub add_v1_1 {
+sub add_interface {
   my ( $self, $interface, $result ) = @_;
   if ( defined $interface and not ref $interface ) {
     return $result->add_interface($interface);
